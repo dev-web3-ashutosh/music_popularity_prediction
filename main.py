2ed2a6f2d44d719e726a6a9255e084f8818632da
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2024-08-17 12:55:11 +0530

    using matplotlib and seaborn to visualize data

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+s_data=pd.read_csv("Spotify_data.csv")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+s_data.drop(columns=['Unnamed: 0'], inplace=True)
+s_data.info()
+features=['Energy', 'Valence', 'Danceability', 'Loudness', 'Acousticness']
+for feature in features:
+    plt.figure(figsize=(8,5))
+    sns.scatterplot(data=s_data, x=feature, y='Popularity')
+    plt.title(f'Popularity vs {feature}')
+    plt.show()
