d548f4cae5bb5af8ac81ae5d2877022ec9fd7ab5
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2024-08-17 12:57:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ s_data=pd.read_csv("Spotify_data.csv")
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 s_data.drop(columns=['Unnamed: 0'], inplace=True)
-s_data.info()
 features=['Energy', 'Valence', 'Danceability', 'Loudness', 'Acousticness']
 for feature in features:
     plt.figure(figsize=(8,5))
